1855e7dd53bc5121f9b85d90f5f035e48f2e226b
aixiaodexiaokulou <13128162982@163.com>
2018-11-01 17:20:36 +0800

    lzy轮播图模型创建

--- axf/models.py
+++ axf/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# 基础类
+class Base(models.Model):
+    img = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    # 商品id
+    trackid = models.CharField(max_length=10)
+    # 不创建基础表
+    class Meta:
+        abstract = True
+
+# 轮播图
+class Wheel(Base):
+    class Meta:
+        db_table = 'axf_wheel'
